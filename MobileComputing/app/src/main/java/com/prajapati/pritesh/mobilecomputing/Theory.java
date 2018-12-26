766f3d96f8da6efccb9a5dca50cc499d7f05208f
PNP4Help <31499171+pnp4help@users.noreply.github.com>
2018-12-26 13:13:02 +0530

    Initial commit

---
+++ MobileComputing/app/src/main/java/com/prajapati/pritesh/mobilecomputing/Theory.java
@@ -0,0 +1,20 @@
+package com.prajapati.pritesh.mobilecomputing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+
+/**
+ * Created by Pritesh on 10-Jun-17.
+ */
+public class Theory extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.theory,container,false);
+    }
+}
